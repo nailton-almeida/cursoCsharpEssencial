2ecacd08b29acc894ef7c1b918dedbc2dd48a74d
NailtonAlmeida <nailton.junior@mc1.com.br>
2023-06-29 11:59:29 -0300

    chore: adicionado chamada de métodos estáticos no arquivo program.cs

--- 05-POO/AtividadesFinaisPOO/Program.cs
+++ 05-POO/AtividadesFinaisPOO/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Exec01.Exec001();
+            Exec02.Exec002();
+            Exec03.Exec003();
             Exec04.Exec004();
         }
     }
